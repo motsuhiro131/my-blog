5dc8bcc6dad02822ea0f9b84addad9f67f7b58ce
motsuhiro131 <yoshihiro.y2033@frontier.hokudai.ac.jp>
2020-04-01 22:17:39 +0900

    0401

--- src/components/bio.js
+++ src/components/bio.js
@@ -58,7 +58,7 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author.name}</strong> {author.summary} \n
+        Written by <strong>{author.name}</strong> {author.summary}
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>Twitter</a>
         /
