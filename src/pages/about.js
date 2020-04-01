614db77fd68a2f813079229b2ba3cf8ae768f3de
motsuhiro131 <yoshihiro.y2033@frontier.hokudai.ac.jp>
2020-04-01 21:33:01 +0900

    0401

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby"
 
 const AboutPage = () => {
     return (
-        <div>
-            <h1>About Page</h1>
-        </div>
-        <h1>Gatsby Blog Site</h1>
+      <div>
+          <ul>
+              <Link to="/about">About</Link>
+          </ul>
+          <h1>Gatsby Blog Site</h1>
+      </div>
     )
 }
 
