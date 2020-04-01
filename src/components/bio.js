88200a4111e1ff653bfd5f4f89b8d33e51db8c08
motsuhiro131 <yoshihiro.y2033@frontier.hokudai.ac.jp>
2020-04-01 20:46:59 +0900

    0401

--- src/components/bio.js
+++ src/components/bio.js
@@ -61,6 +61,7 @@ const Bio = () => {
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>Twitter</a>
         /
+        {` `}
         <a href={`https://github.com/${githubac.username}`}>
           GitHub
         </a>
