4a394fcceff8aafd2570cd0d7c8cba9c90fbfb48
motsuhiro131 <yoshihiro.y2033@frontier.hokudai.ac.jp>
2020-05-03 14:35:44 +0900

    allposts->motsulog

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,13 +48,6 @@ module.exports = {
           `gatsby-remark-smartypants`,
         ],
       },
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `your_space_id`,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: `preview.contentful.com`,
-      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
